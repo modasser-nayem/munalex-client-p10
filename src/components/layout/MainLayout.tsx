3e62e5a16e4cf5faf0b055f07060af459ad7af26
modasser-nayem <modassernayem@gmail.com>
2024-01-25 10:53:19 +0600

    project setup

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,9 @@
+const MainLayout = () => {
+   return (
+      <div className="text-5xl text-center mt-8">
+         Welcome to Munalex Electric gadgets Management Dashboard
+      </div>
+   );
+};
+
+export default MainLayout;
